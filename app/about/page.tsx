7298cced3f6578ebad4b0854046825838d3bf213
hasuhasmath <hasuh5pepe@gmail.com>
2024-07-02 16:31:33 +0530

    initial set up

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+export default function About(){
+    return (
+      <main className="container mx-auto px-4 py-6">
+        <h2 className="text-4xl font-bold mb-4">About Us</h2>
+        <p>
+          Welcome to our blog, your go-to source for insightful articles,
+          updates, and musings on a wide range of topics. Our mission is to
+          provide valuable content that informs, entertains, and inspires our
+          readers
+        </p>
+      </main>
+    );
+}
